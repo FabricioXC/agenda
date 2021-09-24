7f3ffd8337a308404ec8cbaff1241992f7319c82
Fabrício Chiaradia <fabricioxc@gmail.com>
2021-09-24 08:38:52 -0300

    Update

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from core.models import Evento
 
 # Register your models here.
+
+# Definindo os campos que devem ser exibidos referente a tabela (classe)
+# Evento no console de administração.
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'descricao', 'data_evento', 'data_criacao')
+
+# Registrando a classe Evento do core.models e a classe EventoAdmin no admin
+admin.site.register(Evento, EventoAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Evento(models.Model):
+    # verbose_name define o nome que será utilizado para exibição na coluna
+    titulo = models.CharField(max_length=100, verbose_name='Título')
+    descricao = models.TextField(blank=True, null=True, verbose_name='Descrição')
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    # auto_now=True insere a hora atual no campo sempre que for criado um novo registro
+    data_criacao = models.DateTimeField(auto_now=True, verbose_name='Data de Criação')
+
+    # Metadados. Definir o nome da tabela. Caso contrário, o Django escolhe o nome.
+    class Meta: 
+        db_table = 'evento'
+
+    # Sempre que for chamado o objeto título, será trazido o nome do título.
+    # Desta forma, ao invés de aparecer o nome do evento como Evento Object()
+    # no console de administração, o nome trará o título do evento.
+    def __str__(self):
+        return self.titulo
